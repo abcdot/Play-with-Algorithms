e3d8c9b96f58b7cc0ae1ecb38a7d48d748768b78
ilearnit <helloworld.c@outlook.com>
2021-03-01 19:31:33 +0800

    v1.2 add test case

---
+++ SelectionSort/Student.h
@@ -0,0 +1,26 @@
+//
+// Created by Daniel on 2021/3/1.
+//
+
+#ifndef PLAY_WITH_ALGORITHMS_STUDENT_H
+#define PLAY_WITH_ALGORITHMS_STUDENT_H
+
+#include <iostream>
+#include <string>
+
+using  namespace std;
+struct Student {
+    string name;
+    int score;
+    bool operator<(const Student &otherStudent){
+//        return score < otherStudent.score;
+        return score != otherStudent.score ? score < otherStudent.score : name > otherStudent.name;
+    }
+
+    friend ostream& operator << (ostream &os, const Student &student){
+        os << "Student: " << student.name << " " << student.score << endl;
+        return os;
+    }
+};
+
+#endif //PLAY_WITH_ALGORITHMS_STUDENT_H
--- SelectionSort/main.cpp
+++ SelectionSort/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-
+#include <string>
+#include "Student.h"
 // #include <algorithm>
 // 老版本中 swap 在上面的库中
 // 新版本在 std 中
@@ -23,11 +24,39 @@ void selectionSort(T arr[], int n){
 }
 
 int main(){
+
+    // 整型排序
     int a[10] = {10, 9,8,7,6,5,4,3,2,1};
     selectionSort(a, 10);
 
     for (int i = 0; i < 10; ++i) {
         cout << a[i] << " ";
     }
+    cout << endl;
+
+    // 浮点型排序
+    float b[10] = {10.1, 9.2,8.3,7.5,6.2,5.7,4.1,3.3,2.2,1.1};
+    selectionSort(b, 10);
+
+    for (int i = 0; i < 10; ++i) {
+        cout << b[i] << " ";
+    }
+    cout << endl;
+
+    // 字符串排序
+    string c[4] = {"D", "C", "B", "A"};
+    selectionSort(c, 4);
+    for (int i = 0; i < 4; ++i) {
+        cout << c[i] << " ";
+    }
+    cout << endl;
+
+    // 自定义数据类型排序
+    Student d[4] = {{"Leo", 90}, {"Daniel", 99}, {"Echo", 100}, {"Tracy", 99}};
+    selectionSort(d, 4);
+    for (int i = 0; i < 4; ++i) {
+        cout << d[i];
+    }
+    cout << endl;
     return 0;
 }
