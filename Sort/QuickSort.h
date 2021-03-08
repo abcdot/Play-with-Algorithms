9e583267fcdbacf80de004f5239bdf1040096efa
ilearnit <helloworld.c@outlook.com>
2021-03-08 16:31:22 +0800

    refactor quick sort

--- Sort/QuickSort.h
+++ Sort/QuickSort.h
@@ -5,10 +5,16 @@
 #ifndef PLAY_WITH_ALGORITHMS_QUICKSORT_H
 #define PLAY_WITH_ALGORITHMS_QUICKSORT_H
 
+#include <algorithm>
+#include "InsertionSort.h"
+
 // 对 arr[l...r] 部分进行 partition 操作
 // 返回 p，使得 arr[l...p-1] < arr[p]; arr[p+1...r] > arr[p]
 template<typename T>
 int __partition(T arr[], int l, int r){
+
+    // 重点优化二：
+    swap(arr[rand() % (r-l+1)+l], arr[l]);
     T v = arr[l];
 
     // arr[l+1...j] < v; arr[j+1...i] > v
@@ -26,8 +32,14 @@ int __partition(T arr[], int l, int r){
 
 template<typename T>
 void __quickSort(T arr, int l, int r){
-    if(l >= r)
+//    if(l >= r)
+//        return;
+
+    // 优化一：
+    if(r - l <= 15 ){
+        insertionSort(arr, l, r);
         return;
+    }
 
     int p = __partition(arr, l, r);
     __quickSort(arr, p+1, r);
@@ -36,6 +48,103 @@ void __quickSort(T arr, int l, int r){
 
 template<typename T>
 void quickSort(T arr[], int n){
+    // 重点优化二：
+    srand(time(NULL));
     __quickSort(arr, 0, n - 1);
+}
+
+
+// 优化三：二路快速排序
+template<typename T>
+int __partition2(T arr[], int l, int r){
+    swap(arr[rand() % (r-l+1)+l], arr[l]);
+    T v = arr[l];
+
+    // arr[l+1...i) <= v; arr(j...r] >= v
+    int i=l+1, j=r;
+    while (true){
+        while (i <=r && arr[i] < v) i++;
+        while (j >= l+1 && arr[j] > v) j--;
+        if(i > j) break;
+
+        swap(arr[i], arr[j]);
+        i++;
+        j--;
+    }
+
+    swap(arr[l], arr[j]);
+
+    return j;
+}
+
+template<typename T>
+void __quickSort2(T arr, int l, int r){
+//    if(l >= r)
+//        return;
+
+    // 优化一：
+    if(r - l <= 15 ){
+        insertionSort(arr, l, r);
+        return;
+    }
+
+    int p = __partition2(arr, l, r);
+    __quickSort2(arr, p+1, r);
+    __quickSort2(arr, l, p - 1);
+}
+
+template<typename T>
+void quickSort2(T arr[], int n){
+    // 重点优化二：
+    srand(time(NULL));
+    __quickSort2(arr, 0, n - 1);
+}
+
+
+// 优化四： 三路快排
+template<typename T>
+void __quickSort3(T arr[], int l, int r){
+//    if(l >= r)
+//        return;
+
+    // 优化一：
+    if(r - l <= 15 ){
+        insertionSort(arr, l, r);
+        return;
+    }
+
+    // partition
+    swap(arr[rand() % (r-l+1)+l], arr[l]);
+    T v = arr[l];
+
+    int lt = l;  // arr[l+1...lt] < v
+    int gt = r + 1; // arr[gt...r] > v
+    int i = l + 1;  // arr[lt+1...i] == v
+
+    while (i < gt){
+        if(arr[i] < v){
+            swap(arr[i], arr[lt+1]);
+            lt++;
+            i++;
+        } else if (arr[i] > v) {
+            swap(arr[i], arr[gt-1]);
+            gt--;
+        } else { // arr[i] == v
+            i++;
+        }
+    }
+
+    swap(arr[l], arr[lt]);
+
+    __quickSort3(arr, l, lt-1);
+    __quickSort3(arr, gt, r);
+
+}
+
+template<typename T>
+void quickSort3(T arr[], int n){
+    // 重点优化二：
+    srand(time(NULL));
+    __quickSort3(arr, 0, n - 1);
 }
 #endif //PLAY_WITH_ALGORITHMS_QUICKSORT_H
--- Sort/main.cpp
+++ Sort/main.cpp
@@ -18,11 +18,17 @@ int main(){
     int n = 1000000;
 
     // 对于近乎有序数组来说 插入排序要远远的比选择排序效率高
-     int swapTimes = 0;
-//     int *arr = SortTestHelper::generateNearlyOrderedArray(n, swapTimes);
+     int swapTimes = 10;
+    // 测试1 一般性测试
     int *arr = SortTestHelper::generateRandomArray(n, 0, n);
-//    int *arr2 = SortTestHelper::copyIntArray(arr, n);
-//    int *arr3 = SortTestHelper::copyIntArray(arr, n);
+    // 测试2 测试近乎有序的数组
+//     int *arr = SortTestHelper::generateNearlyOrderedArray(n, swapTimes);
+    // 测试3 测试存在包含大量相同元素的数组
+//    int *arr = SortTestHelper::generateRandomArray(n, 0, 10);
+
+
+    int *arr2 = SortTestHelper::copyIntArray(arr, n);
+    int *arr3 = SortTestHelper::copyIntArray(arr, n);
     int *arr4 = SortTestHelper::copyIntArray(arr, n);
 
     cout << "Test for Nearly Ordered Array, size = " << n << ", random range [0, " << n << "]" << endl;
@@ -37,13 +43,17 @@ int main(){
 
 
     // 快排
-
-    SortTestHelper::testSort("Quick Sort", quickSort, arr4, n);
+//    SortTestHelper::testSort("Quick Sort", quickSort, arr4, n);
+    SortTestHelper::testSort("Quick Sort2", quickSort2, arr2, n);
+    SortTestHelper::testSort("Quick Sort3", quickSort3, arr3, n);
+    // 应用： merge sort 取逆序对 nlogn
+    // quick sort 取第 n 大的数 n
 
     delete[] arr;
-//    delete[] arr2;
-//    delete[] arr3;
+    delete[] arr2;
+    delete[] arr3;
     delete[] arr4;
+
 
     return 0;
 }
