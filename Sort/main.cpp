79f926e43bdfe480e905b84c32460c350399fb52
ilearnit <helloworld.c@outlook.com>
2021-03-09 11:55:56 +0800

    init heap sort

--- Sort/main.cpp
+++ Sort/main.cpp
@@ -4,6 +4,7 @@
 #include "SelectionSort.h"
 #include "MergeSort.h"
 #include "QuickSort.h"
+#include "HeapSort.h"
 
 // #include <algorithm>
 // 老版本中 swap 在上面的库中
@@ -15,44 +16,64 @@ int main(){
     // 选择排序测试用例
     // SelectionSortCase();
 
-    int n = 1000000;
+    // int n = 1000000;
 
     // 对于近乎有序数组来说 插入排序要远远的比选择排序效率高
-     int swapTimes = 10;
+    // int swapTimes = 10;
+
     // 测试1 一般性测试
-    int *arr = SortTestHelper::generateRandomArray(n, 0, n);
+    // int *arr = SortTestHelper::generateRandomArray(n, 0, n);
+
     // 测试2 测试近乎有序的数组
-//     int *arr = SortTestHelper::generateNearlyOrderedArray(n, swapTimes);
+    // int *arr = SortTestHelper::generateNearlyOrderedArray(n, swapTimes);
+
     // 测试3 测试存在包含大量相同元素的数组
-//    int *arr = SortTestHelper::generateRandomArray(n, 0, 10);
+    // int *arr = SortTestHelper::generateRandomArray(n, 0, 10);
 
+    // int *arr2 = SortTestHelper::copyIntArray(arr, n);
+    // int *arr3 = SortTestHelper::copyIntArray(arr, n);
+    // int *arr4 = SortTestHelper::copyIntArray(arr, n);
 
-    int *arr2 = SortTestHelper::copyIntArray(arr, n);
-    int *arr3 = SortTestHelper::copyIntArray(arr, n);
-    int *arr4 = SortTestHelper::copyIntArray(arr, n);
-
-    cout << "Test for Nearly Ordered Array, size = " << n << ", random range [0, " << n << "]" << endl;
+    // cout << "Test for Nearly Ordered Array, size = " << n << ", random range [0, " << n << "]" << endl;
     // 时间复杂度 n^2
-//    SortTestHelper::testSort("Insertion Sort", insertionSort, arr, n);
-//    SortTestHelper::testSort("Selection Sort", selectionSort, arr2, n);
+    // SortTestHelper::testSort("Insertion Sort", insertionSort, arr, n);
+    // SortTestHelper::testSort("Selection Sort", selectionSort, arr2, n);
 
     // 时间复杂度 nlogn
     // 对近乎有序的数组会退化到 n^2
-    SortTestHelper::testSort("Merge Sort", mergeSort, arr, n);
-//    SortTestHelper::testSort("Merge Sort BU", mergeSortBU, arr4, n);
+    // SortTestHelper::testSort("Merge Sort", mergeSort, arr, n);
+    // SortTestHelper::testSort("Merge Sort BU", mergeSortBU, arr4, n);
 
 
     // 快排
-//    SortTestHelper::testSort("Quick Sort", quickSort, arr4, n);
-    SortTestHelper::testSort("Quick Sort2", quickSort2, arr2, n);
-    SortTestHelper::testSort("Quick Sort3", quickSort3, arr3, n);
+    // SortTestHelper::testSort("Quick Sort", quickSort, arr4, n);
+    // SortTestHelper::testSort("Quick Sort2", quickSort2, arr2, n);
+    // SortTestHelper::testSort("Quick Sort3", quickSort3, arr3, n);
     // 应用： merge sort 取逆序对 nlogn
     // quick sort 取第 n 大的数 n
 
-    delete[] arr;
-    delete[] arr2;
-    delete[] arr3;
-    delete[] arr4;
+    // delete[] arr;
+    // delete[] arr2;
+    // delete[] arr3;
+    // delete[] arr4;
+
+    // 堆
+    MaxHeap<int> maxHeap = MaxHeap<int>(100);
+    cout << maxHeap.size() << endl;
+
+    srand(time(NULL));
+
+    for (int i = 0; i < 15; ++i) {
+        maxHeap.insert(rand() % 100);
+    }
+
+    maxHeap.testPrint();
+
+    while (!maxHeap.isEmpty()){
+        cout << maxHeap.extractItem() << " ";
+    }
+
+    cout << endl;
 
 
     return 0;
